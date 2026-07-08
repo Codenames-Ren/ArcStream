caad4c45320f97ba09b5686141467a317e997c53
Codenames-Ren <renmizuki38@gmail.com>
2026-07-08 13:02:41 +0700

    add theme configuration shadows.ts

---
+++ src/theme/shadows.ts
@@ -0,0 +1,12 @@
+export const shadows = {
+    card: {
+        shadowColor: '#000',
+        shadowOpacity: 0.15,
+        shadowRadius: 10,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        elevation: 4,
+    },
+} as const;
