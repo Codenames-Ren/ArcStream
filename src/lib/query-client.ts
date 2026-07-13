bfc83e0338559bb716383040ecdffa81405fc692
Codenames-Ren <renmizuki38@gmail.com>
2026-07-14 00:01:04 +0700

    add query clients

---
+++ src/lib/query-client.ts
@@ -0,0 +1,34 @@
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: (failureCount, error: any) => {
+
+                const status =
+                    error?.response?.status;
+
+                if (
+                    status === 429 ||
+                    status === 500 ||
+                    status === 502 ||
+                    status === 503
+                ) {
+                    return false;
+                }
+
+                return failureCount < 1;
+            },
+
+            staleTime: 1000 * 60 * 10,
+
+            gcTime: 1000 * 60 * 30,
+
+            refetchOnReconnect: false,
+
+            refetchOnMount: false,
+
+            refetchOnWindowFocus: false,
+        },
+    },
+});
