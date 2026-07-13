e17d708518d18e718ca153fb8a7131846677174b
Codenames-Ren <renmizuki38@gmail.com>
2026-07-14 00:02:34 +0700

    add query-clients:

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,34 +1,27 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: (failureCount, error: any) => {
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error: any) => {
+        const status = error?.response?.status;
 
-                const status =
-                    error?.response?.status;
+        if (
+          status === 429 ||
+          status === 500 ||
+          status === 502 ||
+          status === 503
+        ) {
+          return false;
+        }
 
-                if (
-                    status === 429 ||
-                    status === 500 ||
-                    status === 502 ||
-                    status === 503
-                ) {
-                    return false;
-                }
-
-                return failureCount < 1;
-            },
-
-            staleTime: 1000 * 60 * 10,
-
-            gcTime: 1000 * 60 * 30,
-
-            refetchOnReconnect: false,
-
-            refetchOnMount: false,
-
-            refetchOnWindowFocus: false,
-        },
+        return failureCount < 1;
+      },
+      staleTime: 1000 * 60 * 10,
+      gcTime: 1000 * 60 * 30,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
     },
+  },
 });
