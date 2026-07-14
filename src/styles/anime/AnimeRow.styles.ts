4faafd0f7c689909e2f680b3b4268acf7859031f
Codenames-Ren <renmizuki38@gmail.com>
2026-07-14 21:03:52 +0700

    add styles anime row component

---
+++ src/styles/anime/AnimeRow.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import { spacing } from '@/theme';
+
+export const animeRowStyles = StyleSheet.create({
+    container: {
+        marginBottom: spacing.xxxl,
+    },
+
+    listContent: {
+        paddingTop: spacing.sm,
+    },
+});
