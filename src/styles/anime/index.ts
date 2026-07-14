225390b2ea7e03ec2e4b3adaae49ddb6ff05cc6c
Codenames-Ren <renmizuki38@gmail.com>
2026-07-14 21:11:12 +0700

    add index styles for exporting all styles

---
+++ src/styles/anime/index.ts
@@ -0,0 +1,10 @@
+export * from "./AnimeCard.styles";
+export * from "./AnimeListItem.style";
+export * from "./AnimeRow.styles";
+export * from "./EpisodeCard.styles";
+export * from "./GenreBadge.styles";
+export * from "./GenreItem.style";
+export * from "./HeroBanner.styles";
+export * from "./HeroCarousel.styles";
+export * from "./ScoreBadge.styles";
+
