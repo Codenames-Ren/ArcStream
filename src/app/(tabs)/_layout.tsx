7ca6f80e8d7b17d58af4e4401f8b18922ee03d9b
Codenames-Ren <renmizuki38@gmail.com>
2026-07-15 13:06:23 +0700

    adjust navigation layout tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,68 +1,16 @@
-import { Feather } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
-
 import { colors } from "@/theme";
+import { Slot } from "expo-router";
+import { View } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-
-        tabBarStyle: {
-          backgroundColor: colors.background,
-          borderTopColor: colors.surface,
-        },
-
-        tabBarActiveTintColor: colors.primary,
-
-        tabBarInactiveTintColor: colors.caption,
-
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontWeight: "600",
-        },
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.background,
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="home" color={color} size={size} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="anime"
-        options={{
-          title: "Anime",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="tv" color={color} size={size} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="genres"
-        options={{
-          title: "Genres",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="tag" color={color} size={size} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "About",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="info" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
+      <Slot />
+    </View>
   );
 }
