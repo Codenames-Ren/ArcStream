9789d547c0470dab4edde9722a25360bdae63e84
Codenames-Ren <renmizuki38@gmail.com>
2026-07-15 12:45:10 +0700

    add styles recommendation component

---
+++ src/styles/anime/RecommendationRow.styles.ts
@@ -0,0 +1,12 @@
+import { spacing } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export const recommendationRowStyles = StyleSheet.create({
+  content: {
+    paddingRight: spacing.lg,
+  },
+
+  itemSeparator: {
+    width: spacing.md,
+  },
+});
