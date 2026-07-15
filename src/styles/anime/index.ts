3052d6bcde5bcf2c095b75a1a06e05402a56f3a7
Codenames-Ren <renmizuki38@gmail.com>
2026-07-15 12:46:53 +0700

    add exporting styles

--- src/styles/anime/index.ts
+++ src/styles/anime/index.ts
@@ -1,10 +1,15 @@
 export * from "./AnimeCard.styles";
+export * from "./AnimeDetailHeader.style";
 export * from "./AnimeListItem.style";
+export * from "./AnimeMeta.style";
 export * from "./AnimeRow.styles";
 export * from "./EpisodeCard.styles";
 export * from "./GenreBadge.styles";
 export * from "./GenreItem.style";
 export * from "./HeroBanner.styles";
 export * from "./HeroCarousel.styles";
+export * from "./RecommendationRow.styles";
+export * from "./RelatedAnimeRow.style";
 export * from "./ScoreBadge.styles";
+export * from "./SynopsisSection.style";
 
