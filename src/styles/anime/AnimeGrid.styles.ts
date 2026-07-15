b70051851511ac70ed9d778d2b9e7777d823a5af
Codenames-Ren <renmizuki38@gmail.com>
2026-07-15 12:46:04 +0700

    adjust styles anime grid components

--- src/styles/anime/AnimeGrid.styles.ts
+++ src/styles/anime/AnimeGrid.styles.ts
@@ -1,12 +1,7 @@
+import { spacing } from "@/theme";
 import { StyleSheet } from "react-native";
 
-import { spacing } from "@/theme";
-
 export const animeGridStyles = StyleSheet.create({
-  container: {
-    paddingHorizontal: spacing.lg,
-  },
-
   content: {
     paddingBottom: spacing.xxxl,
   },
