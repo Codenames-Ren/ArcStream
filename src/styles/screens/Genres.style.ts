e509065736909c5150326aa3ffac6c8afc1d5851
Codenames-Ren <renmizuki38@gmail.com>
2026-07-15 12:51:35 +0700

    adjust syles genre list screen

--- src/styles/screens/Genres.style.ts
+++ src/styles/screens/Genres.style.ts
@@ -1,6 +1,5 @@
-import { StyleSheet } from "react-native";
-
 import { colors, spacing, typhography } from "@/theme";
+import { StyleSheet } from "react-native";
 
 export const genresStyles = StyleSheet.create({
   container: {
@@ -10,33 +9,26 @@ export const genresStyles = StyleSheet.create({
 
   content: {
     padding: spacing.lg,
-    paddingBottom: spacing.xxxl,
+    paddingBottom: spacing.xxxl + 80,
   },
 
   title: {
     color: colors.primary,
-
     fontSize: typhography.h2,
-
     fontWeight: "800",
-
     textAlign: "center",
-
     marginTop: spacing.sm,
   },
 
   subtitle: {
     color: colors.caption,
-
     textAlign: "center",
-
     marginTop: spacing.xs,
     marginBottom: spacing.xl,
   },
 
   row: {
     gap: spacing.md,
-
     marginBottom: spacing.md,
   },
 });
