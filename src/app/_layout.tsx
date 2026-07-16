e3c37002daf925d219ce43f523b4eae29296b441
Codenames-Ren <renmizuki38@gmail.com>
2026-07-16 20:33:15 +0700

    adjust layout root

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,89 +1,124 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import SplashScreen from "@/components/common/SplashScreen";
 import { queryClient } from "@/lib/query-client";
 import { colors } from "@/theme";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import * as ExpoSplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+ExpoSplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   initialRouteName: "(tabs)",
 };
 
 export default function RootLayout() {
-  return (
-    <SafeAreaProvider
-      style={{
-        flex: 1,
-        backgroundColor: colors.background,
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <StatusBar style="light" />
+  const [ready, setReady] = useState(false);
 
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: colors.background,
-            },
+  useEffect(() => {
+    async function prepare() {
+      await ExpoSplashScreen.hideAsync();
+    }
 
-            headerTintColor: colors.text,
+    prepare();
+  }, []);
 
-            headerTitleStyle: {
-              color: colors.text,
-            },
-
-            contentStyle: {
-              backgroundColor: colors.background,
-            },
-
-            animation: "slide_from_right",
-
-            presentation: "card",
-
-            gestureEnabled: true,
-
-            headerShadowVisible: false,
-
-            gestureDirection: "horizontal",
+  if (!ready) {
+    return (
+      <SafeAreaProvider
+        style={{
+          flex: 1,
+          backgroundColor: colors.background,
+        }}
+      >
+        <SplashScreen
+          onFinish={() => {
+            setReady(true);
           }}
-        >
-          <Stack.Screen
-            name="(tabs)"
-            options={{
-              headerShown: false,
-            }}
-          />
+        />
+      </SafeAreaProvider>
+    );
+  }
 
-          <Stack.Screen
-            name="search"
-            options={{
-              title: "Search",
-            }}
-          />
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: colors.background,
+        }}
+        edges={["top", "bottom"]}
+      >
+        <QueryClientProvider client={queryClient}>
+          <StatusBar style="light" />
 
-          <Stack.Screen
-            name="anime/[id]"
-            options={{
-              headerShown: false,
-            }}
-          />
+          <Stack
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: colors.background,
+              },
 
-          <Stack.Screen
-            name="genre/[slug]"
-            options={{
-              headerShown: false,
-            }}
-          />
+              headerTintColor: colors.text,
+              headerTitleStyle: {
+                color: colors.text,
+              },
 
-          <Stack.Screen
-            name="player/[episodeId]"
-            options={{
-              headerShown: false,
+              contentStyle: {
+                backgroundColor: colors.background,
+              },
+
+              animation: "slide_from_right",
+              presentation: "card",
+              gestureEnabled: true,
+              headerShadowVisible: false,
+              gestureDirection: "horizontal",
             }}
-          />
-        </Stack>
-      </QueryClientProvider>
+          >
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerShown: false,
+              }}
+            />
+
+            <Stack.Screen
+              name="search"
+              options={{
+                title: "Search",
+              }}
+            />
+
+            <Stack.Screen
+              name="anime/[id]"
+              options={{
+                headerShown: false,
+              }}
+            />
+
+            <Stack.Screen
+              name="genre/[slug]"
+              options={{
+                headerShown: false,
+              }}
+            />
+
+            <Stack.Screen
+              name="player/[episodeId]"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="list/[type]"
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack>
+        </QueryClientProvider>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
