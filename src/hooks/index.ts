efb4e1d9eccf0716aa2912bbbaff756eacb8413f
Codenames-Ren <renmizuki38@gmail.com>
2026-07-16 20:35:19 +0700

    register completed hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./useAnimeDetail";
 export * from "./useAnimeList";
+export * from "./useCompletedAnime";
 export * from "./useGenres";
 export * from "./useHome";
 export * from "./useSearchAnime";
