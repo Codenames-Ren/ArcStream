e5d116de42038c1ef80ec060c07b0fffe1c77461
Codenames-Ren <renmizuki38@gmail.com>
2026-07-16 20:42:58 +0700

    registering custom splash screen styles to index

--- src/styles/common/index.ts
+++ src/styles/common/index.ts
@@ -2,6 +2,7 @@ export * from "./AlphabetFIlter.styles";
 export * from "./CatLoader.styles";
 export * from "./DetailNavigation.style";
 export * from "./EmptyState.styles";
+export * from "./EpisodeNavigation.style";
 export * from "./ErrorState.styles";
 export * from "./GenreDetail.styles";
 export * from "./NetworkImage.styles";
@@ -9,5 +10,9 @@ export * from "./PageJump.style";
 export * from "./Pagination.styles";
 export * from "./SearchBar.styles";
 export * from "./SectionTitle.styles";
+export * from "./ServerSelector.styles";
 export * from "./Skeleton.styles";
+export * from "./SplashScreen.styles";
+export * from "./VideoPlayes.style";
+export * from "./WebPlayer.style";
 
