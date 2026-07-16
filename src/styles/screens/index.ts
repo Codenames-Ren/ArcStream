6525c0f7964fd048875c524b7b6880e28f969324
Codenames-Ren <renmizuki38@gmail.com>
2026-07-16 20:41:03 +0700

    adding to index screen styles

--- src/styles/screens/index.ts
+++ src/styles/screens/index.ts
@@ -4,4 +4,7 @@ export * from "./AnimeDetail.style";
 export * from "./GenreDetail.style";
 export * from "./Genres.style";
 export * from "./Home.style";
+export * from "./List.styles";
+export * from "./Player.style";
+export * from "./Search.styles";
 
